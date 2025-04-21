c68cedc365f5d7e00f059f38b119d5d72c4d5086
W2CH <modeb993@gmail.com>
2025-04-21 01:23:55 -0700

    Finished get all customers api

--- Repository.js
+++ Repository.js
@@ -12,6 +12,9 @@ class Repository {
   insertAccount(account_type, balance,customer_id){
     return this.dao.run(queries.addAccount, [account_type,balance,customer_id]);
   }
+  allCustomers(){
+    return this.dao.run(queries.getCustomers);
+  }
 
   createCustomerTable() {
     const sql = queries.createCustomerTable;
